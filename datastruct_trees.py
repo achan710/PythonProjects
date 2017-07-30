e84dfb268f3af44111fef3dab438224c5752b8f8
Allan Chan <allanchansf@gmail.com>
2017-07-30 15:23:02 -0700

    version 2

--- datastruct_trees.py
+++ datastruct_trees.py
@@ -25,45 +25,65 @@ class Node():
 	def getleft(self):
 		return self.left
 	
+	def display(self):
+		print '---start---'
+		print self.key
+		print self.left
+		print self.right
+		print '---end---'
+	
 
 class BinarySearchTree():
 	
 	def __init__(self):
 		self.root = None
 
-
 	def add(self, key):
-		self.__insert(self.root, key)
+		# pitfall: had to add a case to handle when root is None
+		
+		if self.root == None:
+			self.root = Node(key)
+		else:
+			self.__insert(self.root, key)
 
 	def walktree(self):
 		self.__traverse(self.root)
 
 	def __insert(self, node, key):
+	
+		print key
+		#if node is not None:
+			#node.display()
+		
 		if node == None:
 			# found position to insert node
 			node = Node(key)
-			print node.key
-			return
-
-		# search left if key is less than, or right if key is greater than
-		left = node.left
-		if key < left.key:
-			self.__insert(left, key)
+			#print node.key
+			
 		else:
-			self.__insert(node.right, key)
+			# pitfall: originally was checking left.key. But left does not exist yet
+			
+			# search left if key is less than, or right if key is greater than
+			if key < node.getkey():
+				self.__insert(node.getleft(), key)
+			else:
+				self.__insert(node.getright(), key)
 		
 	def __traverse(self, node):
 		if node == None:
 			return
 		
-		self.__traverse(node.left)
+		self.__traverse(node.getleft())
 		print node.key
-		self.__traverse(node.right)
+		self.__traverse(node.getright())
 	
 	def debug(self):
-		print self.root
-		
-if __name__ == '__main__':
+		print '\n'
+		print self.root.key
+		print self.root.left
+		print self.root.right
+
+def test_node():
 
 	a = Node(5)
 	b = Node(6)
@@ -72,14 +92,22 @@ if __name__ == '__main__':
 	items = [a,b,c]
 	output = ' '.join( [str(x.key) for x in items] )
 	print output
-	
+
+def test_bst():
 	T = BinarySearchTree()
 	T.add(5)
 	T.add(6)
 	T.add(2)
-	#T.debug()
 	T.walktree()
+	
+	T.debug()
 
+
+if __name__ == '__main__':
+
+	#test_node()
+		
+	test_bst()
 
 
 	
